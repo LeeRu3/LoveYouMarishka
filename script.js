3058885c5388736d352e6b612bd7cbf61473b610
LeeRu3 <137153120+LeeRu3@users.noreply.github.com>
2024-05-04 08:36:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// Находим кнопку "воспоминания" и модальное окно по их id
+const memoriesBtn = document.getElementById('memoriesBtn');
+const memoriesModal = document.getElementById('memoriesModal');
+
+// На кнопку "воспоминания" навешиваем обработчик события click
+memoriesBtn.addEventListener('click', function() {
+    memoriesModal.style.display = 'block'; // Показываем модальное окно
+});
+
+// Находим крестик в модальном окне воспоминаний
+const memoriesCloseBtn = memoriesModal.querySelector('.close');
+
+// На крестик также навешиваем обработчик события click
+memoriesCloseBtn.addEventListener('click', function() {
+    memoriesModal.style.display = 'none'; // Закрываем модальное окно
+});
+
+// Находим кнопку "открыть подарок" и модальное окно по их id
+const giftBtn = document.getElementById('giftBtn');
+const giftModal = document.getElementById('giftModal');
+
+// На кнопку "открыть подарок" навешиваем обработчик события click
+giftBtn.addEventListener('click', function() {
+    giftModal.style.display = 'block'; // Показываем модальное окно
+});
+
+// Находим крестик в модальном окне подарка
+const giftCloseBtn = giftModal.querySelector('.close');
+
+// На крестик также навешиваем обработчик события click
+giftCloseBtn.addEventListener('click', function() {
+    giftModal.style.display = 'none'; // Закрываем модальное окно
+});
+
 // Находим кнопку "секрет" и навешиваем на неё обработчик события click
 const secretBtn = document.getElementById('secretBtn');
 
@@ -6,6 +40,7 @@ secretBtn.addEventListener('click', function() {
     // Создаем модальное окно с вопросом и кнопками
     const secretModal = document.createElement('div');
     secretModal.classList.add('modal');
+    secretModal.style.display = 'block'; // Устанавливаем модальному окну стиль display: block;
     const modalContent = document.createElement('div');
     modalContent.classList.add('modal-content');
     modalContent.style.background = 'linear-gradient(to bottom, #ffe6f2, #ffcce6)'; // Градиент переливающийся пастельных тонов
